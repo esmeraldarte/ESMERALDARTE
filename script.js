fe48064de7a13ebdc86605cf0083870e7ab61010
esmeraldarte <andresfelipegrcmrtnz@gmail.com>
2025-08-07 19:06:06 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function buscarCertificado() {
+    const codigo = document.getElementById('codigoBusqueda').value;
+    alert('Buscar: ' + codigo);
+}
+function descargarPDF() {
+    alert('Función de descarga de PDF en construcción');
+}
